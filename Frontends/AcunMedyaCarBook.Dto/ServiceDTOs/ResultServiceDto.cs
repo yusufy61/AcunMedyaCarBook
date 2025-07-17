86731e5df79e28427a9ac6c8e7c4db233445affe
Yusuf YILDIZ <yusufyildiz@ogrenci.beykoz.edu.tr>
2025-07-17 17:50:35 +0300

    add services controller  with component for mvc

---
+++ Frontends/AcunMedyaCarBook.Dto/ServiceDTOs/ResultServiceDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcunMedyaCarBook.Dto.ServiceDTOs
+{
+    public class ResultServiceDto
+    {
+        public int servicesID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string iconUrl { get; set; }
+    }
+}
--- Frontends/AcunMedyaCarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/AcunMedyaCarBook.WebUI/Controllers/ServiceController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcunMedyaCarBook.Dto.ServiceDTOs;
+using AcunMedyaCarBook.Dto.TestimonialDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace AcunMedyaCarBook.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7236/api/Services");
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+            return View(values);
         }
     }
 }
